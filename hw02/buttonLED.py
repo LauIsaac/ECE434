232c664b12efa7c161aff967d93a52a3e9b1f903
Demo User <debian@beaglebone.localdomain>
2019-09-15 23:44:11 +0000

    Completed part 1 of hw, blinking LED with buttons

---
+++ hw02/buttonLED.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+buttonClear = 'P9_11'
+buttonLeft  = 'P9_13' 
+buttonDown  = 'P9_15'
+buttonUp    = 'P9_27'
+buttonRight = 'P8_17'
+
+LEDleft     = 'P8_7'
+LEDup       = 'P8_9'
+LEDdown     = 'P8_11'
+LEDright    = 'P8_13'
+
+
+map = {buttonLeft: LEDleft, buttonUp: LEDup, buttonDown: LEDdown, buttonRight: LEDright} #
+
+GPIO.setup(LEDleft,     GPIO.OUT)
+GPIO.setup(LEDup,       GPIO.OUT)
+GPIO.setup(LEDdown,     GPIO.OUT)
+GPIO.setup(LEDright,    GPIO.OUT)
+
+GPIO.setup(buttonLeft,  GPIO.IN)
+GPIO.setup(buttonDown,  GPIO.IN)
+GPIO.setup(buttonUp,    GPIO.IN)
+GPIO.setup(buttonRight, GPIO.IN)
+
+def updateLED(channel):
+    state = GPIO.input(channel)
+    GPIO.output(map[channel], state)
+    
+    
+GPIO.add_event_detect(buttonRight,  GPIO.BOTH, callback=updateLED)
+GPIO.add_event_detect(buttonLeft,   GPIO.BOTH, callback=updateLED)
+GPIO.add_event_detect(buttonUp,     GPIO.BOTH, callback=updateLED)
+GPIO.add_event_detect(buttonDown,   GPIO.BOTH, callback=updateLED)
+    
+try:
+    
+    while True:
+        time.sleep(100)
+        
+except KeyboardInterrupt:
+    print("Cleaning Up")
+    GPIO.cleanup()
